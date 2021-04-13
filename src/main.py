a2e97230c71987e460dfc88f347fb5e17263b04e
adriancastro626 <adriancastro626@icloud.com>
2021-04-13 01:04:20 +0000

    update

--- src/main.py
+++ src/main.py
@@ -8,17 +8,30 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet
+import json
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
 #from models import Person
 
 app = Flask(__name__)
+
+
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["JWT_SECRET_KEY"] = os.environ.get('FLASK_APP_KEY')  # Change this!
+
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+
+jwt = JWTManager(app)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -30,14 +43,78 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+@app.route("/login", methods=["POST"])
+def login_token():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    # Query your database for username and password
+    user = User.query.filter_by(username=username, password=password).first()
+    if user is None:
+        # the user was not found on the database
+        return jsonify({"msg": "Bad username or password"}), 401
+    
+    # create a new token with the user id inside
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id })
+
+
+
+
+###############################################################
 @app.route('/user', methods=['GET'])
-def handle_hello():
+@jwt_required()
+def get_user():
+   
+    return jsonify(User.getAllusers()), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user/<int:id>', methods=['GET'])
+@jwt_required()
+def get_Oneuser(id): 
+    current_user_id = get_jwt_identity()
+    user = User.filter.get(current_user_id)
+    return jsonify(User.getUser(id)), 200    
 
-    return jsonify(response_body), 200
+@app.route('/user', methods=['POST'])
+def new_user():
+    request_body_user=request.data
+    decoded_object = json.loads(request_body_user)
+    return jsonify(User.create_user(decoded_object)), 200
+
+@app.route('/user/<int:id>/newChar/<int:char_id>', methods=['PUT'])
+def new_favChar(id, char_id):
+    return jsonify(User.newFavChar(id, char_id))
+
+@app.route('/user/<int:id>/newPlanet/<int:planet_id>', methods=['PUT'])
+def new_favPlanet(id, planet_id):
+    return jsonify(User.newFavPlanet(id, planet_id))
+
+@app.route('/user/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    return jsonify(User.deleteUser(id)), 200
+
+#########################################################
+@app.route('/character', methods=['GET'])
+def get_character():
+    return jsonify(Character.getAllcharacters()), 200
+
+@app.route('/character/<int:id>', methods=['GET'])
+def get_OneChar(id):
+    return jsonify(Character.getChar(id)), 200  
+
+# @app.route('/character', methods=['POST'])
+# def new_character():
+#     request_body_char=request.data
+#     decoded_object = json.loads(request_body_char)
+#     return jsonify(Character.create_character(decoded_object)), 200
+
+###########################################################
+@app.route('/planet', methods=['GET'])
+def get_planet():
+    return jsonify(Planet.getAllplanets()), 200
+
+@app.route('/planet/<int:id>', methods=['GET'])
+def get_OnePlanet(id):
+    return jsonify(Planet.getPlanet(id)), 200  
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,12 +1,28 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify, url_for
 
 db = SQLAlchemy()
 
+fav_char = db.Table('fav_chars', db.metadata, 
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id")), 
+    db.Column("char_id", db.Integer, db.ForeignKey("characters.id")))
+
+fav_planet = db.Table('fav_planets', db.metadata, 
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id")), 
+    db.Column("plan_id", db.Integer, db.ForeignKey("planets.id")))
+
 class User(db.Model):
+    __tablename__='users'
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    lastname = db.Column(db.String(250), nullable=False)
+    username=db.Column(db.String(250), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
+    characters = db.relationship("Character", secondary=fav_char, back_populates="users") 
+    planets = db.relationship("Planet", secondary=fav_planet, back_populates="users") 
+    
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +31,140 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "username":self.username,
+            "characters": list(map(lambda x: x.serialize(), self.characters)),
+            "planets": list(map(lambda x: x.serialize(), self.planets))
             # do not serialize the password, its a security breach
         }
+    
+    def getAllusers():
+        users_query = User.query.all()
+        all_users = list(map(lambda x: x.serialize(), users_query))
+        return(all_users)
+    
+    def getUser(id):
+        user_query = User.query.get(id)
+        return(user_query.serialize())
+
+
+    def create_user(request_body_user):
+        user = User(name=request_body_user["name"], lastname=request_body_user["lastname"], username=request_body_user["username"], email=request_body_user["email"], password=request_body_user["password"])
+        db.session.add(user)
+        db.session.commit()
+        return("An user has been added")
+
+    def deleteUser(id):
+        user = User.query.get(id)
+        if user is None:
+            raise APIException('User not found', status_code=404)
+        db.session.delete(user)
+        db.session.commit()
+        return("The user has been deleted")
+
+    def newFavChar(id, char_id):
+        user = User.query.get(id)
+        char = Character.query.get(char_id) 
+        if user is None:
+            raise APIException('User not found', status_code=404)
+        if char is None:
+            raise APIException('Character not found', status_code=404)
+       
+        user.characters.append(char)
+        db.session.commit()
+        return user.serialize()
+    
+    def newFavPlanet(id, planet_id):
+        user = User.query.get(id)
+        planet = Planet.query.get(planet_id) 
+        if user is None:
+            raise APIException('User not found', status_code=404)
+        if planet is None:
+            raise APIException('Planet not found', status_code=404)
+       
+        user.planets.append(planet)
+        db.session.commit()
+        return user.serialize()
+
+
+class Character(db.Model):
+    __tablename__='characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    birth_year = db.Column(db.String(250), nullable=False)
+    gender=db.Column(db.String(250), nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    hair_color=db.Column(db.String(250), nullable=False)
+    eye_color=db.Column(db.String(250), nullable=False)
+    
+    
+    users = db.relationship("User", secondary=fav_char, back_populates="characters") 
+   
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "height": self.height,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color
+            # do not serialize the password, its a security breach
+        }
+    
+    def getAllcharacters():
+        characters_query = Character.query.all()
+        all_characters = list(map(lambda x: x.serialize(), characters_query))
+        return(all_characters)
+    
+    def getChar(id):
+        char_query = Character.query.get(id)
+        return(char_query.serialize())
+
+    # def create_character(request_body_char):
+    #     char1 = Character(name=request_body_char["name"], birth_year=request_body_char["birth_year"], gender=request_body_char["gender"], height=request_body_char["height"], eye_color=request_body_char["eye_color"], hair_color=request_body_char["hair_color"])
+    #     db.session.add(char1)
+    #     db.session.commit()
+    #     return("A character has been added")
+
+class Planet(db.Model):
+    __tablename__='planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    climate = db.Column(db.String(250), nullable=False)
+    population = db.Column(db.String(250), nullable=False)
+    orbital_period = db.Column(db.Integer, nullable=False)
+    rotation_period = db.Column(db.Integer, nullable=False)
+    diameter = db.Column(db.Integer, nullable=False)
+
+    users = db.relationship("User", secondary=fav_planet, back_populates="planets") 
+    
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate":self.climate,
+            "population":self.population,
+            "orbital_period":self.orbital_period,
+            "rotation_period":self.rotation_period,
+            "diameter":self.diameter  
+            
+            # do not serialize the password, its a security breach
+        }
+    
+    def getAllplanets():
+        planets_query = Planet.query.all()
+        all_planets = list(map(lambda x: x.serialize(), planets_query))
+        return(all_planets)
+
+    def getPlanet(id):
+        planet_query = Planet.query.get(id)
+        return(planet_query.serialize())
+
+    # def create_planet(request_body_planet):
+    #     planet = Planet(name=request_body_planet["name"], population=request_body_planet["population"], gender=request_body_char["gender"], height=request_body_char["height"], eye_color=request_body_char["eye_color"], hair_color=request_body_char["hair_color"])
+    #     db.session.add(planet)
+    #     db.session.commit()
+    #     return("A planet has been added")
+    
+    
